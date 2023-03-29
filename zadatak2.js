7a3d933f45f7d93a7aa151e19ece5721d017f20d
AntonioEdelinski <95490631+AntonioEdelinski@users.noreply.github.com>
2023-03-29 19:11:24 +0200

    Add files via upload

---
+++ zadatak2.js
@@ -0,0 +1,29 @@
+function DaniUTjednu{
+    var Dan = 0;
+    switch (Dan) {
+        case 1:
+            console.log("Ponedjeljak");
+            break;
+        case 2:
+            console.log("Utorak");
+            break;
+        case 3:
+            console.log("Srijeda");
+            break;
+        case 4:
+            console.log("Četvrtak");
+            break;
+        case 5:
+            console.log("Petak");
+            break;
+        case 6:
+            console.log("Subota");
+            break;
+        case 7:
+            console.log("Nedelja")
+        default:
+            break;
+    }
+
+};
+
