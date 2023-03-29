596a15e9e876b9b855ecbd8faddacea70bf444e3
AntonioEdelinski <95490631+AntonioEdelinski@users.noreply.github.com>
2023-03-29 19:33:38 +0200

    Add files via upload

---
+++ zadatak4.js
@@ -0,0 +1,18 @@
+const somePeople = {
+    Donald: "Knuth",
+    Bjarne: "Stroustrup",
+    Ada: "Lovelace",
+    Kent: "Beck",
+    Martin: "Fowler",
+    Erich: "Gamma"
+}
+
+function Najmanji(){
+    for (let i = 0; i < somePeople.length; i++) {
+        if(somePeople){
+            
+        }
+        
+    }
+        
+    }
